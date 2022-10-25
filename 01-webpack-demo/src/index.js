7edbefe903385ec419c0967db42d65d32918e9d1
hucongcong <flycc258@163.com>
2022-10-25 20:25:24 +0800

    webpack基本架子

---
+++ 01-webpack-demo/src/index.js
@@ -0,0 +1,4 @@
+const div = document.createElement('div')
+div.innerHTML = '<h1>hello webpack！</h1>'
+
+document.querySelector('#app').appendChild(div)
---
+++ 01-webpack-demo/webpack.config.js
@@ -0,0 +1,23 @@
+// 打包的时候需要注释，增加webpack的提示
+// import { Configuration } from 'webpack'
+
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+/**
+ * @type { Configuration }
+ */
+const config = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
+}
+
+module.exports = config
