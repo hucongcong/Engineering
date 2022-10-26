251176ee2bc883273bc885b8b9bae8c113d68752
hucongcong <flycc258@163.com>
2022-10-26 20:19:28 +0800

    pc改造-mini-css-extract-plugin抽离css

--- 02-ZBestPC/webpack.config.js
+++ 02-ZBestPC/webpack.config.js
@@ -2,6 +2,7 @@
 
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const webpack = require('webpack')
 /**
  * @type {Configuration}
@@ -17,7 +18,7 @@ const config = {
     path: path.join(__dirname, 'dist')
   },
   mode: 'development',
-  devtool: 'source-map',
+  // devtool: 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
@@ -32,34 +33,36 @@ const config = {
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery'
+    }),
+    new MiniCssExtractPlugin({
+      filename: './css/[name].css'
     })
   ],
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
       {
         test: /\.(png|jpe?g|svg|gif)$/i,
         type: 'asset',
         parser: {
           dataUrlCondition: {
             // 小于20kb的生成base64
-            maxSize: 4 * 1024,
+            maxSize: 4 * 1024
           }
         },
         generator: {
-          filename: 'img/[name].[hash].[ext]',
-        },
+          filename: 'img/[name].[hash].[ext]'
+        }
       },
       {
         test: /\.html$/,
         use: 'html-loader'
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
   }
-  
 }
 
 module.exports = config
